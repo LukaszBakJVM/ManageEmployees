8be2b389122a32ae4bf0cd91dfcbbf37de9b2491
bbzzyyczczeek <bbzzyyczczeek@interia.pl>
2023-07-22 14:08:27 +0200

    Time Work

---
+++ src/main/java/com/example/manageemployees/Employee/Work/WorkDto.java
@@ -0,0 +1,46 @@
+package com.example.manageemployees.Employee.Work;
+
+
+
+import java.time.LocalDateTime;
+
+public class WorkDto {
+    private long id;
+    private LocalDateTime startWork;
+
+    private LocalDateTime endWork;
+    private long timeWork;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getStartWork() {
+        return startWork;
+    }
+
+    public void setStartWork(LocalDateTime startWork) {
+        this.startWork = startWork;
+    }
+
+    public LocalDateTime getEndWork() {
+        return endWork;
+    }
+
+    public void setEndWork(LocalDateTime endWork) {
+        this.endWork = endWork;
+    }
+
+    public long getTimeWork() {
+        return timeWork;
+    }
+
+    public void setTimeWork(long timeWork) {
+        this.timeWork = timeWork;
+    }
+}
