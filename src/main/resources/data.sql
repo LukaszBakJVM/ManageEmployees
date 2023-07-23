6a1396f530a6f441e620c560ab1a928150c0821c
bbzzyyczczeek <bbzzyyczczeek@interia.pl>
2023-07-23 12:43:59 +0200

    Add ContractType

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,10 @@
 insert into Contract_Type (Rodzaj_Umowy) values ('Umowa o prace');
 insert into Contract_Type (Rodzaj_Umowy) values ('Umowa zlecenie');
 insert into Contract_Type (Rodzaj_Umowy) values ('B2b');
+
+
+insert into employee (Imie, Nazwisko, Wiek,dochod,start_work,end_work,time_work,Contract_Type_id) values ('Silvana', 'Hinkley', 20,0,null,null,0,1);
+insert into Employee (Imie, Nazwisko, Wiek,dochod,start_work,end_work,time_work,Contract_Type_id) values ('Tuck', 'Dansey', 20,0,null,null,0,1);
+insert into Employee (Imie, Nazwisko, Wiek,dochod,start_work,end_work,time_work,Contract_Type_id) values ('Rory', 'Earles', 20,0,null,null,0,2);
+insert into Employee (Imie, Nazwisko, Wiek,dochod,start_work,end_work,time_work,Contract_Type_id) values ('Zia', 'Matanin', 20,0,null,null,0,2);
+insert into Employee (Imie, Nazwisko, Wiek,dochod,start_work,end_work,time_work,Contract_Type_id) values ('Danny', 'Beaver', 20,0,null,null,0,3);
