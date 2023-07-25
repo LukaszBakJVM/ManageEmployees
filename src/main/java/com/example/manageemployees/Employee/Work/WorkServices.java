7f6001342250f44673a10d109607b4f12e6d71f0
bbzzyyczczeek <bbzzyyczczeek@interia.pl>
2023-07-25 17:16:21 +0200

    Work time calculation

--- src/main/java/com/example/manageemployees/Employee/Work/WorkServices.java
+++ src/main/java/com/example/manageemployees/Employee/Work/WorkServices.java
@@ -7,23 +7,34 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
+import java.time.temporal.ChronoUnit;
+
 @Service
 public class WorkServices {
+
+
 
 
     private final EmployeeRepository repository;
 
     public WorkServices( EmployeeRepository repository) {
-
         this.repository = repository;
     }
     @Transactional
     void start(long id){
+
+
         Employee employee = repository.findById(id).orElseThrow();
+        long timeWork =employee.getTimeWork();
        if (employee.getStartWork()==null){
            employee.setStartWork(LocalDateTime.now());
        }else {
            employee.setEndWork(LocalDateTime.now());
+           long between = ChronoUnit.MINUTES.between(employee.getStartWork(), employee.getEndWork());
+           timeWork +=between;
+           employee.setTimeWork(timeWork);
+           employee.setStartWork(null);
+           employee.setEndWork(null);
        }
     }
 
