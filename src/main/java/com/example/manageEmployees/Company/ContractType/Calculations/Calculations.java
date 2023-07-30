63b725890f9d20164f0462c031bf9efadf8ffce8
bbzzyyczczeek <bbzzyyczczeek@interia.pl>
2023-07-30 19:00:21 +0200

    Payout Calculations

--- src/main/java/com/example/manageEmployees/Company/ContractType/Calculations/Calculations.java
+++ src/main/java/com/example/manageEmployees/Company/ContractType/Calculations/Calculations.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Calculations {
     private double ZUS;
+    private final double VAT_23=0.23;
 
     double UoP(long timeWork,int age,double rate){
         double payment;
@@ -12,6 +13,7 @@ public class Calculations {
             payment=rate;
 
         }else {
+            ZUS+=(rate*0.1);
             payment=rate-(rate*0.1);
         }
 
@@ -19,12 +21,21 @@ public class Calculations {
 
     }
 
-     double UZ (long timeWork,int age){
-        return 0;
+     double UZ (long timeWork,int age,double rate){
+         double payment=rate*timeWork;
+        if (age<=26){
+          return payment;
+
+        }
+        ZUS+=payment*0.20;
+
+
+        return payment-(payment*0.2);
 
     }
-    double B2b(long timeWork){
-return  0;
+    double B2b(long timeWork,double rate){
+        double payment =timeWork*rate;
+return payment+(payment*VAT_23)  ;
     }
 
 
